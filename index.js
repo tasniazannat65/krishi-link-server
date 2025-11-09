cd8fed9819d024b9d9d994d54690939867e1c9f5
tasniazannat65 <tasniazannat9@gmail.com>
2025-11-09 22:15:56 +0600

    feat(api): Add GET /crops endpoint to fetch all crop data

--- index.js
+++ index.js
@@ -25,8 +25,16 @@ app.get('/', (req, res)=>{
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db('fasalbridge_db');
+    const cropsCollection = db.collection('crops');
+   app.get('/crops', async(req, res)=>{
+    const cursor = cropsCollection.find();
+    const result = await cursor.toArray();
+    res.send(result);
+   })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
