1f992fc25b012aedc6885b15c32fff6c5dbc9d67
tasniazannat65 <tasniazannat9@gmail.com>
2025-11-09 20:02:05 +0600

    feat(db): Create MongoDB user for database authentication

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERS}:${process.env.DB_PASSWORD}@cluster0.gwyrdqg.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
 app.get('/', (req, res)=>{
-    res.send('krishi-link server is running successfully');
+    res.send('fasal-bridge server is running successfully');
 })
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.listen(port, ()=>{
-    console.log(`krishi-link server is running on port: ${port}`);
+    console.log(`fasal-bridge server is running on port: ${port}`);
     
 })
